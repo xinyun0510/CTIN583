4000a66c5a578dc45837241b7db184ba542764cc
SophieShen-0627 <sophie.s.00510@gmail.com>
2023-10-16 15:38:21 -0700

    include webbuild

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+  loaderUrl: "./webglBuildTest/Build/webglBuildTest.loader.js",
+  dataUrl: "./webglBuildTest/Build/webglBuildTest.data",
+  frameworkUrl: "./webglBuildTest/Build/webglBuildTest.framework.js",
+  codeUrl: "./webglBuildTest/Build/webglBuildTest.wasm",
+});
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className ="Unity">
+        <Unity unityContext={unityContext} 
+        style= {{
+          height: "100vh",
+          width: "100%",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          justifySelf: "center",
+          alignSelf: "center",
+          overflow: "hidden",
+        }}/>  
+      </div>
+
+      <div className="HeaderBar">
+        <h1>WebGLbuildTest</h1>
+      </div>
     </div>
   );
 }
